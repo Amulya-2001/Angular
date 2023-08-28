c60ac73ca88f04c194f1eebfd3c0332d94b24eaf
Amulya-2001 <68948045+Amulya-2001@users.noreply.github.com>
2023-08-28 20:08:31 +0000

    Update data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -42,47 +42,5 @@ export class DataService {
 }
 }
 
-  /*
-  getValues(): Observable<string[]> {
-    return this.http.get<string[]>(this.baseUrl);
-  }
-
-  addValue(value: string): Observable<any> {
-    this.values.push(value);
-    return this.http.put(this.baseUrl, this.values);
-  }
-
-  updateValue(index: number, updatedValue: string): Observable<any> {
-    // Update the value in the in-memory array
-    this.values[index] = updatedValue;
-  
-    // Prepare the data for the update request
-    const updatedData = this.values;
-  
-    // Make the PUT request to update the data on the server
-    return this.http.put(this.baseUrl, updatedData);
-  }
-  
-
-  deleteValue(index: number): Observable<any> {
-    this.values.splice(index, 1);
-    return this.http.put(this.baseUrl, this.values);
-  }
-
- 
-  private values:string[]=['AMULYA','BINDU','SANJANA','HUSNA','GOWTHAMI'];
-
-  private validCredentials=[
-    {Username:'userOne',Password:'pwOne'},
-    {Username:'userTwo',Password:'pwTwo'},
-    {Username:'userThree',Password:'pwThree'},
-  ];
-
-  authenticate(username:string,password:string):boolean{
-    const validUser=this.validCredentials.find(
-      (cred)=>cred.Username===username && cred.Password===password
-    );
-    return !!validUser;
-  }*/
 
 
